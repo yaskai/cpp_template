9588716c5d69f058fa9354dafe682ab12618b719
Kai Yasko <yasko.kai@gmail.com>
2025-08-05 23:34:17 -0400

    out.sh fix

--- out.sh
+++ out.sh
@@ -1,1 +1,11 @@
-./bin/Debug/water
+dir_name=$(pwd)
+shopt -s extglob
+proj_name=${dir_name%%+(/)}
+proj_name=${proj_name##*/}   
+proj_name=${proj_name:-/}    
+
+cd ./build &&
+./premake5 gmake2 &&
+cd .. &&
+make &&
+./bin/Debug/$proj_name
